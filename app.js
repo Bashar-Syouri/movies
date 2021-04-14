51e77045ad388aa9be79338ef9b12ef3b502f91b
BasharNezam <>
2021-04-14 20:26:13 +0000

    edit

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log('hello');
+
+console.log('Welcome to my Page');
+
+
+
+alert('Welcome To My Movie Page ' )
+
+
+var userName = prompt("please enter your name");
+
+
+var timeNow = prompt("please enter the time now");
+
+if (timeNow >= 18) {
+  document.write("Good Evening");
+  document.write(
+    '<img src="https://i.ytimg.com/vi/mxRgBBIacGc/hqdefault.jpg">'
+  );
+} else if (timeNow >= 12) {
+  document.write("Good Afternoon");
+  document.write(
+    '<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/14ee75b7-d544-4bc9-993f-854eac083213/d5y1m7g-e8569699-de6e-4850-a7a8-1db8df702fe9.png/v1/fill/w_1153,h_693,q_70,strp/beautiful_evening_by_shini_smurf_d5y1m7g-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MDgiLCJwYXRoIjoiXC9mXC8xNGVlNzViNy1kNTQ0LTRiYzktOTkzZi04NTRlYWMwODMyMTNcL2Q1eTFtN2ctZTg1Njk2OTktZGU2ZS00ODUwLWE3YTgtMWRiOGRmNzAyZmU5LnBuZyIsIndpZHRoIjoiPD0xMzQzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.1F8tnCzXJLcPBUged-lG5z8ynok1uQrq7kMkMqP0vEY">'
+  );
+} else if (timeNow >= 0) {
+  document.write("Good Morning");
+  document.write(
+    '<img src="https://i.pinimg.com/originals/48/47/62/484762b63c2a8458c6accc211e1e6fd8.png">'
+  );
+}
+
+
+
+  
